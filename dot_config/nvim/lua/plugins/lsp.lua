7b6d4558e35be90e45416e622bbf44496da32e29
Sangyeop Kim <sangykimiv@gmail.com>
2025-07-04 17:00:13 +0900

    Update .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/markdown-preview.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -12,21 +12,21 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {
-          on_init = function(client)
-            local path = client.workspace_folders[1].name
-            local venv_python = path .. "/.venv/bin/python"
-
-            -- 유효한 가상환경인지 확인
-            local stat = vim.loop.fs_stat(venv_python)
-            if stat and stat.type == "file" then
-              client.config.settings = {
-                python = {
-                  pythonPath = venv_python,
-                },
-              }
-              client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-            end
-          end,
+          -- on_init = function(client)
+          --   local path = client.workspace_folders[1].name
+          --   local venv_python = path .. "/.venv/bin/python"
+          --
+          --   -- 유효한 가상환경인지 확인
+          --   local stat = vim.loop.fs_stat(venv_python)
+          --   if stat and stat.type == "file" then
+          --     client.config.settings = {
+          --       python = {
+          --         pythonPath = venv_python,
+          --       },
+          --     }
+          --     client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          --   end
+          -- end,
           settings = {
             python = {
               venvPath = ".",
---
+++ dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "psymoney/markdown-preview.nvim",
+    branch = "master",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}
