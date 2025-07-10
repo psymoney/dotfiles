47c5b7685ef77b4d6be2146dde1e07e1230618ff
Sangyeop Kim <sangykimiv@gmail.com>
2025-07-11 06:22:31 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- auto format on save
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  pattern = "*",
-  command = "silent! write",
-})
