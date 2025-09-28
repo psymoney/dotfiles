1c8fd689ec32c6a951fd1d90491d7141f56905fd
Sangyeop Kim <sangykimiv@gmail.com>
2025-09-28 13:29:25 +0900

    Remove .config/nvim/lua/plugins/copilot-chat.lua Update .config/nvim/lua/plugins/tokyonight.lua

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -3,13 +3,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      dim_inactive = false,
-      on_highlights = function(hl, c)
-        hl.LineNr = { fg = c.fg_dark } -- Use main foreground color (bright)
-        hl.LineNrAbove = { fg = c.fg_dark } -- Lines above cursor
-        hl.LineNrBelow = { fg = c.fg_dark } -- Lines below cursor
-      end,
-    },
+    -- opts = {
+    --   dim_inactive = false,
+    --   on_highlights = function(hl, c)
+    --     hl.LineNr = { fg = c.fg_dark } -- Use main foreground color (bright)
+    --     hl.LineNrAbove = { fg = c.fg_dark } -- Lines above cursor
+    --     hl.LineNrBelow = { fg = c.fg_dark } -- Lines below cursor
+    --   end,
+    -- },
   },
 }
